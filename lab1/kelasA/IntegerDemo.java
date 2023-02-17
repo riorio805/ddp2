a93346891204787a4116f2198fcce1d0da842db9
riorio805 <sefrianojieftara@gmail.com>
2023-02-17 08:43:08 +0700

    Lab 01 kelas A

---
+++ lab1/kelasA/IntegerDemo.java
@@ -0,0 +1,20 @@
+// part 1
+
+public class IntegerDemo {
+    public static void main(String[] args)
+    {
+        System.out.println("Maximum int value: " + Integer.MAX_VALUE);
+        System.out.println("Minimum int value: " + Integer.MIN_VALUE);
+        //Add statements to print the minimum and maximum values for
+        //byte, short and long.
+
+        System.out.println("Maximum byte value: " + Byte.MAX_VALUE);
+        System.out.println("Minimum byte value: " + Byte.MIN_VALUE);
+
+        System.out.println("Maximum short value: " + Short.MAX_VALUE);
+        System.out.println("Minimum short value: " + Short.MIN_VALUE);
+
+        System.out.println("Maximum long value: " + Long.MAX_VALUE);
+        System.out.println("Minimum long value: " + Long.MIN_VALUE);
+    }
+}
