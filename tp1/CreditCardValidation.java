5be75c6d760d5b6f36e82e932d9a0c5b92b1e53e
riorio805 <70353501+riorio805@users.noreply.github.com>
2023-02-20 12:22:24 +0700

    Delete tp1 directory

--- tp1/CreditCardValidation.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class CreditCardValidation {
-    public static void main (String[] args) {
-        JFrame frame = new JFrame();
-        JOptionPane.showMessageDialog(frame,"hello world");
-    }
-}
