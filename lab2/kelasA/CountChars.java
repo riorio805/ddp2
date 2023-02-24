91ff5489745f5618fac8194032dfa1b1acd55e4f
riorio805 <sefrianojieftara@gmail.com>
2023-02-24 14:44:50 +0700

    cleanup + add flair

--- lab2/kelasA/CountChars.java
+++ lab2/kelasA/CountChars.java
@@ -8,9 +8,12 @@ public class CountChars {
             int countA = 0, countE = 0, countI = 0, countO = 0, countU = 0,
                 countSpace = 0, countConsonant = 0, countOther = 0;
             // take input from user
-            String input = JOptionPane.showInputDialog(null, "Enter a sentence of phrase, quit to end:");
-            // break if user inputs "quit"
-            if (input.equals("quit")) break;
+            String input = JOptionPane.showInputDialog(null,
+                "Enter a sentence of phrase, quit to end:\n",
+                "Character Counter", JOptionPane.PLAIN_MESSAGE);
+            // break if user inputs "quit" or clicks cancel (input == null)
+            if (input == null || input.equals("quit")) break;
+
 
             // loop through each character
             for (int i = 0; i < input.length(); i++) {
@@ -32,20 +35,22 @@ public class CountChars {
                 }
             }
 
+
             // show the totals to user (using String.format)
             JOptionPane.showMessageDialog(null, String.format("""
-                    There are:
-                    %d blank spaces,
-                    %d A,
-                    %d E,
-                    %d I,
-                    %d O,
-                    %d U,
-                    %d consonants,
-                    %d other characters
-                    in the phrase "%s".
-                    """, countSpace, countA, countE, countI, countO, countU, countConsonant, countOther, input));
-
+                        There are:
+                        %d blank spaces,
+                        %d A,
+                        %d E,
+                        %d I,
+                        %d O,
+                        %d U,
+                        %d consonants,
+                        %d other characters
+                        in the phrase "%s".
+                        """, countSpace, countA, countE, countI, countO,
+                             countU, countConsonant, countOther, input)
+                ,"Totals", JOptionPane.PLAIN_MESSAGE);
         }
     }
 }
