9ffb51420b29026c211b3e1c9e0648a83c06ad71
riorio805 <sefrianojieftara@gmail.com>
2023-03-01 09:42:25 +0700

    with break

--- worksheets/ws01/Worksheet1.java
+++ worksheets/ws01/Worksheet1.java
@@ -66,14 +66,14 @@ class Worksheet1 {
 
     // no break
     public static void Problem8a (int day) {
-        switch (day%7) {
-            case 0: System.out.println("Sunday");
-            case 1: System.out.println("Monday");
-            case 2: System.out.println("Tuesday");
-            case 3: System.out.println("Wednesday");
-            case 4: System.out.println("Thursday");
-            case 5: System.out.println("Friday");
-            case 6: System.out.println("Saturday");
+        switch (day % 7) {
+            case 0: System.out.println("Sunday"); break;
+            case 1: System.out.println("Monday"); break;
+            case 2: System.out.println("Tuesday"); break;
+            case 3: System.out.println("Wednesday"); break;
+            case 4: System.out.println("Thursday"); break;
+            case 5: System.out.println("Friday"); break;
+            case 6: System.out.println("Saturday"); break;
             default: System.out.println("not a day");
         }
     }
