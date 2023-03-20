dc77a2c57f5ae87a4cd678748e3c46efff2e3ed5
riorio805 <sefrianojieftara@gmail.com>
2023-03-20 09:15:38 +0700

    lab 5 kelasA final

--- lab5/kelasA/ArrayProcessing.java
+++ lab5/kelasA/ArrayProcessing.java
@@ -4,14 +4,21 @@ public class ArrayProcessing {
     static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[] intArray = new int[10];
-        String selection = "";
+        // Initialize array and selection
+        int[] intArray = new int[0];
+        String selection;
 
-        while (! selection.equals("0")) {
+        do { // while (! selection.equals("0"))
+            // Print menu and get input
             printMenu();
             selection = input.nextLine();
+
+            // Switch depending on input
             switch (selection) {
+                // Quit program
                 case "0" -> System.out.println("Bye!");
+
+                //
                 case "1" -> {
                     int elementAmount;
                     System.out.println("How many elements?");
@@ -49,11 +56,18 @@ public class ArrayProcessing {
                 }
                 default -> System.out.println("Sorry, invalid choice");
             }
-        }
+        } while (! selection.equals("0"));
 
         input.close();
     }
 
+    /**
+     Method to generate an int Array of input length where every element
+     is an integer from 1 to 1000 inclusive
+
+     @param length the length of the array
+     @return an integer Array with random numbers.
+     */
     public static int[] generateArray (int length) {
         int[] out = new int[length];
         for (int i = 0; i < length; i++) {
@@ -62,22 +76,37 @@ public class ArrayProcessing {
         return out;
     }
 
-    public static int linearSearch (int[] array, int n) {
-        return linearSearchRec(array, n, 0);
-    }
+    /**
+     Method to search for a target in
+     @param array input array
+     @param n target integer to find in array
+     @return -1 if target not in array, position of the target otherwise.
+     */
+    public static int linearSearch (int[] array, int n) {return linearSearchRec(array, n, 0);}
 
+    /**
+     Recursive method to find the target in an array using linear search.
+     */
     private static int linearSearchRec (int[] array, int n, int index) {
         if (index == array.length) return -1;
         if (array[index] == n) return index;
         return linearSearchRec(array, n, ++index);
     }
 
+    /**
+     Method to sort the input array using selection sort
+     @param array the array to be sorted
+     */
     public static void selectionSort (int[] array) {
         for (int sortedIdx = 0; sortedIdx < array.length - 1; sortedIdx++) {
+
             int minIndex = sortedIdx;
+            
             for (int nextIdxs = sortedIdx + 1; nextIdxs < array.length; nextIdxs++) {
                 if (array[minIndex] > array[nextIdxs]) minIndex = nextIdxs;
             }
+
+            // Swap to the lowest if current element is not the lowest
             if (minIndex != sortedIdx) {
                 int temp = array[minIndex];
                 array[minIndex] = array[sortedIdx];
@@ -86,6 +115,9 @@ public class ArrayProcessing {
         }
     }
 
+    /**
+     Method to print the menu of the program
+     */
     public static void printMenu() {
         System.out.print("""
             
